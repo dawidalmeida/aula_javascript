98292df883d0316b75be11d9f7676e3aee5e140d
David Ferreira <89822784+dawidalmeida@users.noreply.github.com>
2021-09-23 22:41:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+function clicou(){
+document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+}
+
+function redirecionar(){
+
+  window.open("http://blackdesert-forums.playredfox.com/index.php?tags/global-labs/");
+  window.location.href = "http://blackdesert-forums.playredfox.com/index.php?tags/global-labs/";
+}
+
+function trocar(){
+alert("trocar texto");
+
+}
+/*console.log(document.getElementById("agradecimento"));
+}
+
+  /*  alert("Obrigado por clicar");
+
+}/*
+
+/*
+function soma(n1,n2){
+  return n1 + n2;
+
+}*/
+/*
+function setReplace(frase, nome, novo_nome){
+
+   return frase.replace(nome, novo_nome)
+}
+alert(soma(5, 10));
+alert(setReplace("Vai japão" , "Japão", "Brasil"));
+*/
+/* 
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+/*
+/*var count = 0;
+for(count=0; count <= 5; count++){
+    alert(count);
+
+};
+
+*/
+/*var count = 0;
+while (count <=  5){
+     console.log(count);
+     alert(count);
+     count = count + 1;
+
+}
+*/
+/*
+var idade = prompt("Qual sua idade");
+
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+var lista = ["maça","pêra", "laranja"];
+console.log(lista);
+
+//var nome = "David Ferreira";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão e o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//console.long(nome);
+//console.long(n1 * n2);
+//console.long(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil" ));
